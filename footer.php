3950b8a87b7ac49d57ff10f57440b697dcde54ce
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-04-16 00:49:10 +0300

    Update
    Shopping page updated

---
+++ footer.php
@@ -0,0 +1,110 @@
+<div class="f-widget"><!--footer Widget-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4"><!--footer twitter widget-->
+					<div class="title-widget-bg">
+						<div class="title-widget">Twitter Updates</div>
+					</div>
+					<ul class="tweets">
+						<li>Check out this great #themeforest item for you
+						'Simpler Landing' <a href="#">http://t.co/LbLwldb6 </a>
+						<span>2 hours ago</span></li>
+						<li class="lastone">Check out this great #themeforest item for you
+						'Simpler Landing' <a href="#">http://t.co/LbLwldb6 </a>
+						<span>2 hours ago</span></li>
+					</ul>
+					<div class="clearfix"></div>
+					<a href="#" class="btn btn-default btn-follow"><i class="fa fa-twitter fa-2x"></i><div>Follow us on twitter</div></a>
+				</div><!--footer twitter widget-->
+				<div class="col-md-4"><!--footer newsletter widget-->
+					<div class="title-widget-bg">
+						<div class="title-widget">Newsletter Signup</div>
+					</div>
+					<div class="newsletter">
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</p>
+						<form role="form">
+							<div class="form-group">
+								<label>Your Email address</label>
+								<input type="email" class="form-control newstler-input" id="exampleInputEmail1" placeholder="Enter email">
+								<button class="btn btn-default btn-red btn-sm">Sign Up</button>
+							</div>
+						</form>
+					</div>
+				</div><!--footer newsletter widget-->
+				<div class="col-md-4"><!--footer contact widget-->
+					<div class="title-widget-bg">
+						<div class="title-widget-cursive">Shopping</div>
+					</div>
+					<ul class="contact-widget">
+						<li class="fphone">+387 123 456, +387 123 456 <br> +387 123 456</li>
+						<li class="fmobile">+387-123-456-1<br>+387-123-456-2</li>
+						<li class="fmail lastone">your@email.com<br>customer.care@mail.com</li>
+					</ul>
+				</div><!--footer contact widget-->
+			</div>
+			<div class="spacer"></div>
+		</div>
+	</div><!--footer Widget-->
+	<div class="footer"><!--footer-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-9">
+					<ul class="footermenu"><!--footer nav-->
+						<li><a href="index-1.htm">Home</a></li>
+						<li><a href="cart.htm">My Cart</a></li>
+						<li><a href="checkout.htm">Checkout</a></li>
+						<li><a href="order.htm">Completed Orders</a></li>
+						<li><a href="contact.htm">Contact us</a></li>
+					</ul><!--footer nav-->
+					<div class="f-credit">&copy;All rights reserved by <a href="#">yoursite.com</a></div>
+					<a href=""><div class="payment visa"></div></a>
+					<a href=""><div class="payment paypal"></div></a>
+					<a href=""><div class="payment mc"></div></a>
+					<a href=""><div class="payment nh"></div></a>
+				</div>
+				<div class="col-md-3"><!--footer Share-->
+					<div class="followon">Follow us on</div>
+					<div class="fsoc">
+						<a href="http://twitter.com/minimalthemes" class="ftwitter">twitter</a>
+						<a href="http://www.facebook.com/pages/Minimal-Themes/264056723661265" class="ffacebook">facebook</a>
+						<a href="#" class="fflickr">flickr</a>
+						<a href="#" class="ffeed">feed</a>
+						<div class="clearfix"></div>
+					</div>
+					<div class="clearfix"></div>
+				</div><!--footer Share-->
+			</div>
+		</div>
+	</div><!--footer-->
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="bootstrap\js\bootstrap.min.js"></script>
+	
+	<!-- map -->
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script> 
+	<script type="text/javascript" src="js\jquery.ui.map.js"></script>
+	<script type="text/javascript" src="js\demo.js"></script>
+	
+	<!-- owl carousel -->
+    <script src="js\owl.carousel.min.js"></script>
+	
+	<!-- rating -->
+	<script src="js\rate\jquery.raty.js"></script>
+	<script src="js\labs.js" type="text/javascript"></script>
+	
+	<!-- Add mousewheel plugin (this is optional) -->
+	<script type="text/javascript" src="js\product\lib\jquery.mousewheel-3.0.6.pack.js"></script>
+	
+	<!-- fancybox -->
+    <script type="text/javascript" src="js\product\jquery.fancybox.js?v=2.1.5"></script>
+	
+	<!-- custom js -->
+    <script src="js\shop.js"></script>
+	</div>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,399 @@
+<?php 
+require_once 'header.php';
+?>
+
+<div class="container">
+		<ul class="small-menu"><!--small-nav -->
+			<li><a href="" class="myacc">My Account</a></li>
+			<li><a href="" class="myshop">Shopping Chart</a></li>
+			<li><a href="" class="mycheck">Checkout</a></li>
+		</ul><!--small-nav -->
+		<div class="clearfix"></div>
+		<div class="lines"></div>
+		<div class="main-slide">
+			<div id="sync1" class="owl-carousel">
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Stylish Jacket, be your best deal</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-1.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Stylish Jacket, be your best deal</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-2.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Nike Airmax</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-3.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Unique smaragd ring</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-4.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Stylish Jacket, be your best deal</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-2.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				<div class="item">
+					<div class="slide-desc">
+						<div class="inner">
+							<h1>Nike Airmax</h1>
+							<p>
+								Nunc non fermentum nunc. Sed ut ante eget leo tempor consequat sit amet eu orci. Donec dignissim dolor eget..
+							</p>
+							<button class="btn btn-default btn-red btn-lg">Add to cart</button>
+						</div>
+						<div class="inner">
+							<div class="pro-pricetag big-deal">
+								<div class="inner">
+										<span class="oldprice">$314</span>
+										<span>$199</span>
+										<span class="ondeal">Best Deal</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="slide-type-1">
+						<img src="images\slide-3.jpg" alt="" class="img-responsive">
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div class="bar"></div>
+		<div id="sync2" class="owl-carousel">
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Stylish Jacket</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Stylish Jacket</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Nike Airmax</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Unique smaragd ring</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Stylish Jacket</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+			<div class="item">
+				<div class="slide-type-1-sync">
+					<h3>Nike Airmax</h3>
+					<p>Description here here here</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="f-widget featpro">
+		<div class="container">
+			<div class="title-widget-bg">
+				<div class="title-widget">Featured Products</div>
+				<div class="carousel-nav">
+					<a class="prev"></a>
+					<a class="next"></a>
+				</div>
+			</div>
+			<div id="product-carousel" class="owl-carousel owl-theme">
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<div class="hot"></div>
+							<a href="product.htm"><img src="images\sample-1.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Nikon Camera</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<div class="new"></div>
+							<a href="product.htm"><img src="images\sample-2.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag on-sale"><div class="inner on-sale"><span class="onsale"><span class="oldprice">$314</span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Black Shoes</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-3.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Red T-Shirt</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-1.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Nikon Camera</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-2.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Black Shoes</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-3.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Red T-Shirt</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+				<div class="item">
+					<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-1.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag blue"><div class="inner"><span>$199</span></div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Nikon Camera</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9"><!--Main content-->
+				<div class="title-bg">
+					<div class="title">About Shopping</div>
+				</div>
+				<p class="ct">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<p class="ct">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+				<a href="" class="btn btn-default btn-red btn-sm">Read More</a>
+				
+				<div class="title-bg">
+					<div class="title">Lastest Products</div>
+				</div>
+				<div class="row prdct"><!--Products-->
+					<div class="col-md-4">
+						<div class="productwrap">
+							<div class="pr-img">
+								<a href="product.htm"><img src="images\sample-2.jpg" alt="" class="img-responsive"></a>
+								<div class="pricetag on-sale"><div class="inner on-sale"><span class="onsale"><span class="oldprice">$314</span>$199</span></div></div>
+							</div>
+							<span class="smalltitle"><a href="product.htm">Black Shoes</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-1.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag"><div class="inner">$199</div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Nikon Camera</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-3.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag"><div class="inner">$199</div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Red T- Shirt</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-1.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag"><div class="inner">$199</div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Nikon Camera</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-2.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag"><div class="inner">$199</div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Black Shoes</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="productwrap">
+						<div class="pr-img">
+							<a href="product.htm"><img src="images\sample-3.jpg" alt="" class="img-responsive"></a>
+							<div class="pricetag"><div class="inner">$199</div></div>
+						</div>
+							<span class="smalltitle"><a href="product.htm">Red T-Shirt</a></span>
+							<span class="smalldesc">Item no.: 1000</span>
+						</div>
+					</div>
+				</div><!--Products-->
+				<div class="spacer"></div>
+			</div><!--Main content-->
+			<div class="col-md-3"><!--sidebar-->
+				<div class="title-bg">
+					<div class="title">Categories</div>
+				</div>
+				
+				<div class="categorybox">
+					<ul>
+						<li><a href="category.htm">Women Accessories</a></li>
+						<li><a href="category.htm">Men Shoes</a></li>
+						<li><a href="category.htm">Gift Specials</a></li>
+						<li><a href="category.htm">Electronics</a>
+							<ul>
+								<li><a href="#">iPhone 4S</a></li>
+								<li><a href="#">Samsung Galaxy</a></li>
+								<li><a href="#">MacBook Pro 17"</a></li>
+							</ul>
+						</li>
+						<li><a href="category.htm">On sale</a></li>
+						<li><a href="category.htm">Summer Specials</a></li>
+						<li><a href="category.htm">Electronics</a></li>
+						<li class="lastone"><a href="category.htm">Unique Stuff</a></li>
+					</ul>
+				</div>
+				
+				<div class="ads">
+					<a href="product.htm"><img src="images\ads.png" class="img-responsive" alt=""></a>
+				</div>
+				
+			</div><!--sidebar-->
+		</div>
+	</div>
+
+<?php 
+require_once 'footer.php';
+?>
