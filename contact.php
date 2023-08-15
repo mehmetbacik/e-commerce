0de7da16606a4badfc23bbc304d3ae963c7bf1b5
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-08-15 16:18:09 +0300

    Update
    Created contact page and mailing form.

---
+++ contact.php
@@ -0,0 +1,72 @@
+﻿<?php require_once 'header.php' ?>
+
+<div class="container">
+	<div class="clearfix"></div>
+	<div class="lines"></div>
+</div>
+<div class="container">
+	<div class="row">
+	</div>
+	<div class="title-bg">
+		<div class="title">Contact</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<p class="page-content">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis architecto corrupti, 
+				cupiditate dolores eaque in molestiae officia vero impedit? Ipsum, tempora ipsa corporis 
+				aliquid sed amet qui numquam distinctio explicabo.
+			</p>
+			<ul class="contact-widget">
+				<li class="fphone"><?php echo  $settingbring['setting_tel'] ?></li>
+				<li class="fmobile"><?php echo  $settingbring['setting_gsm'] ?></li>
+				<li class="fmail lastone"><?php echo  $settingbring['setting_mail'] ?></li>
+			</ul>
+		</div>
+		<div class="col-md-7 col-md-offset-1">
+			<!-- Google Maps - Go to the bottom of the page to change settings and map location. -->
+			<div id="googlemaps" class="google-map google-map-footer">
+				<iframe
+				width="100%"
+				height="100%"
+				frameborder="0" style="border:0"
+				src="https://www.google.com/maps/embed/v1/place?key=<?php echo $settingbring['setting_goooglemap']; ?>
+				&q=<?php echo $settingbring['setting_adress']; ?>" allowfullscreen>
+			</iframe>
+		</div>
+	</div>
+</div>
+<div class="title-bg">
+	<div class="title">Contact Form</div>
+</div>
+<div class="qc">
+	<form action="http://www.domain.com.tr/folder/mailphp/mail.php" method="POST" role="form">
+		<div class="form-group">
+			<label for="name">Name Surname<span>*</span></label>
+			<input type="text" name="namesurname" class="form-control" id="name">
+		</div>
+		<div class="form-group">
+			<label for="email">Mail<span>*</span></label>
+			<input type="email" name="email" class="form-control" id="email">
+		</div>
+		<div class="form-group">
+			<label for="text">Message<span>*</span></label>
+			<textarea name="mesaj" class="form-control" id="text"></textarea>
+		</div>
+		<?php 
+		$n1=rand(10,30);
+		$n2=rand(0,10);
+		$total=$n1+$n2;
+		?>
+		<input type="hidden" value="<?php echo $total; ?>" name="total">
+		<div class="form-group">
+			<label for="name">Answer?<span>*</span></label>
+			<input type="text" name="process"  placeholder="<?php echo $n1." + ".$n2. " = ?";  ?>" class="form-control" id="process">
+		</div>
+		<button type="submit" class="btn btn-default btn-red btn-sm">Send</button>
+	</form>
+</div>
+<div class="spacer"></div>
+</div>
+
+<?php require_once 'footer.php' ?>
---
+++ mailphp/mail.php
@@ -0,0 +1,43 @@
+<?
+include '../../admin/system/connect.php';
+require("class.phpmailer.php");
+
+if ($_POST['total']!=$_POST['process']) {
+	echo "Bot control";
+	exit;
+} {
+	//We are pulling our mail information from Mysql.
+	$settingcontrol=$db->prepare("SELECT * FROM setting WHERE setting_id=?");
+	$settingcontrol->execute(array(0));
+	$settingbring=$settingcontrol->fetch(PDO::FETCH_ASSOC);
+
+
+	$mail = new PHPMailer();
+	$mail->IsSMTP();  
+	$mail->CharSet="SET NAMES UTF8";  // send via SMTP
+	$mail->Host     = $settingbring['setting_smtphost']; // SMTP servers
+	$mail->SMTPAuth = true;     // turn on SMTP authentication
+	$mail->Username = $settingbring['setting_smtpuser'];  // SMTP username
+	$mail->Password = $settingbring['setting_smtppassword'];// SMTP password
+	$mail->Port     = $settingbring['setting_smtpport'];
+	$mail->From     = $settingbring['setting_smtpuser']; // Must be the same as your SMTP username
+	$mail->Fromname = "Project Test Mail";
+	//Duplicate this line for multiple mails
+	$mail->AddAddress("mail@domain.com.tr","Form Mail");
+	
+	$mail->Subject  =  $_POST['namesurname'];
+	$mail->Body     =  implode("    ",$_POST);
+
+if(!$mail->Send())
+{
+	echo "Message could not be sent";
+	echo "Mailer Error: " . $mail->ErrorInfo;
+	exit;
+}
+
+echo "Message Sent";
+exit;
+}
+
+?>
+<!--<meta http-equiv="refresh" content="0;URL=../contact.php?status=success">-->
